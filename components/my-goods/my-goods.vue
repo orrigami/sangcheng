dc6015a07cd5e077b29f270ba4ebe7c857142ace
origami <1358992717@qq.com>
2022-06-10 12:59:11 +0800

    完成商品列表

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+  <view class="goods-item">
+    <!-- 商品左侧图片区域 -->
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 商品右侧信息区域 -->
+    <view class="goods-item-right">
+      <!-- 商品标题 -->
+      <view class="goods-name">{{goods.goods_name}}</view>
+      <view class="goods-info-box">
+        <!-- 商品价格 -->
+        <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    // 定义 props 属性，用来接收外界传递到当前组件的数据
+    props: {
+      // 商品的信息对象
+      goods: {
+        type: Object,
+        defaul: {},
+      },
+    },
+    data() {
+      return {
+        // 默认图片
+        defaultPic: 'https://img0.baidu.com/it/u=2570981049,4015989895&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=400'
+      }
+    },
+    filters: {
+      // 把数字处理为带两位小数点的数字
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .goods-item-left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
+</style>
