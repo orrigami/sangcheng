22ffe3a5f0a52f018a3bdf4e46c7fb0349b73a5f
origami <1358992717@qq.com>
2022-06-10 19:23:20 +0800

    完成购物车开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,6 +2,7 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+      <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
       <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
     </view>
     <!-- 商品右侧信息区域 -->
@@ -11,6 +12,7 @@
       <view class="goods-info-box">
         <!-- 商品价格 -->
         <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+        <uni-number-box :min="1" :value="goods.goods_count" v-if="showNum" @change="numChangeHandler"></uni-number-box>
       </view>
     </view>
   </view>
@@ -25,11 +27,44 @@
         type: Object,
         defaul: {},
       },
+      // 是否展示图片左侧的 radio
+      showRadio: {
+        type: Boolean,
+        // 如果外界没有指定 show-radio 属性的值，则默认不展示 radio 组件
+        default: false
+      },
+      // 是否展示价格右侧的 NumberBox 组件
+      showNum: {
+        type: Boolean,
+        default: false,
+      },
     },
     data() {
       return {
         // 默认图片
         defaultPic: 'https://img0.baidu.com/it/u=2570981049,4015989895&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=400'
+      }
+    },
+    methods: {
+      // radio 组件的点击事件处理函数
+      radioClickHandler() {
+        // 通过 this.$emit() 触发外界通过 @ 绑定的 radio-change 事件，
+        // 同时把商品的 Id 和 勾选状态 作为参数传递给 radio-change 事件处理函数
+        this.$emit('radio-change', {
+          // 商品的 Id
+          goods_id: this.goods.goods_id,
+          // 商品最新的勾选状态
+          goods_state: !this.goods.goods_state
+        })
+      },
+      numChangeHandler(val) {
+        // console.log(val)
+        this.$emit('num-change', {
+          // 商品的 Id
+          goods_id: this.goods.goods_id,
+          // 商品的最新数量
+          goods_count: +val
+        })
       }
     },
     filters: {
@@ -43,12 +78,19 @@
 
 <style lang="scss">
   .goods-item {
+    // 让 goods-item 项占满整个屏幕的宽度
+    width: 750rpx;
+    // 设置盒模型为 border-box
+    box-sizing: border-box;
     display: flex;
     padding: 10px 5px;
     border-bottom: 1px solid #f0f0f0;
 
     .goods-item-left {
       margin-right: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
       .goods-pic {
         width: 100px;
@@ -61,14 +103,21 @@
       display: flex;
       flex-direction: column;
       justify-content: space-between;
+      flex: 1;
 
       .goods-name {
         font-size: 13px;
       }
 
-      .goods-price {
-        font-size: 16px;
-        color: #c00000;
+      .goods-info-box {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .goods-price {
+          font-size: 16px;
+          color: #c00000;
+        }
       }
     }
   }
